98b040bab0eaaee3eeff05edcddffa1ffbc3dc7c
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-04-03 23:02:27 +0300

    Main ui state updated.

--- lab2-16/src/jvmMain/kotlin/MainUiState.kt
+++ lab2-16/src/jvmMain/kotlin/MainUiState.kt
@@ -3,5 +3,6 @@ import java.io.File
 data class MainUiState(
     val inputFile: File = File(""),
     val outputFile: File = File(""),
-    val startValue: String = ""
+    val startValue: String = "",
+    val keyVisibility: Boolean = false
 )
