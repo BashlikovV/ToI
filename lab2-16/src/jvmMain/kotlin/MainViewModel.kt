e76b808bc3773aa5ccd512a486a5c14a92cdd970
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-04-03 23:02:45 +0300

    Main view modelupdated.

--- lab2-16/src/jvmMain/kotlin/MainViewModel.kt
+++ lab2-16/src/jvmMain/kotlin/MainViewModel.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.flow.update
 import java.awt.FileDialog
 import java.awt.Frame
 import java.io.File
+import java.util.*
 import javax.swing.JOptionPane
 
 class MainViewModel {
@@ -11,23 +12,51 @@ class MainViewModel {
     private val _mainUiState = MutableStateFlow(MainUiState())
     val mainUiState = _mainUiState.asStateFlow()
 
+    companion object {
+        const val POLYNOMIAL_POWER = 38
+    }
+
+    private val encoder = Encoder()
+    val key = encoder.bufGenkey
+
     fun onStartValueChange(newValue: String) {
         _mainUiState.update { it.copy(startValue = newValue) }
     }
 
-    fun onProcessClicked() {
+    private fun preprocess(str: String): String {
+        val upper = str.uppercase(Locale.getDefault())
+        return upper.filter { it in "01" }
+    }
 
+    fun onProcessClicked() {
+        val outputPath = _mainUiState.value.outputFile.absolutePath
+        val inputPath = _mainUiState.value.inputFile.absolutePath
+        val key = preprocess(_mainUiState.value.startValue)
+
+        if (inputPath.isEmpty() || outputPath.isEmpty() || key.length != POLYNOMIAL_POWER) {
+            showMessage("Please fill in all the fields")
+            return
+        }
+
+        encoder.encode(
+            polynomialPowers = intArrayOf(POLYNOMIAL_POWER, 6, 5, 1),
+            initialKey = key,
+            pathToSrcFile = _mainUiState.value.inputFile.absolutePath,
+            pathToResFile = _mainUiState.value.outputFile.absolutePath
+        )
+
+        showMessage("Success")
     }
 
     fun onOpenKeyClicked() {
-
+        _mainUiState.update { it.copy(keyVisibility = !it.keyVisibility) }
     }
 
     fun onSelectFileClicked(field: Int) {
         val dialog = FileDialog(
             Frame(),
             if (field == 0) "Select input file" else "Select output file",
-            FileDialog.LOAD
+            if (field == 0) FileDialog.LOAD else FileDialog.SAVE
         )
         dialog.isVisible = true
         val path = dialog.directory + dialog.file
@@ -54,7 +83,7 @@ class MainViewModel {
         }
     }
 
-    fun showMessage(message: String) {
+    private fun showMessage(message: String) {
         JOptionPane.showMessageDialog(Frame(), message)
     }
 }
