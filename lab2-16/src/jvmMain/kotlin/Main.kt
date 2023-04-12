9bbfd2b601c0373003bd00dfa77cfd1bb70f428a
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-04-12 22:01:52 +0300

    Plain text and cipher text bits view added.

--- lab2-16/src/jvmMain/kotlin/Main.kt
+++ lab2-16/src/jvmMain/kotlin/Main.kt
@@ -1,13 +1,17 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.rememberScrollableState
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
@@ -104,7 +108,12 @@ fun App(mainViewModel: MainViewModel) {
                     )
                 }
             }
-            KeyWindow(mainUiState.keyVisibility, mainViewModel.key.toString()) {
+            KeyWindow(
+                initialState = mainUiState.keyVisibility,
+                key = mainViewModel.bufGenkey,
+                srcBytes = mainViewModel.bufSrcFile,
+                resBytes = mainViewModel.bufResFile
+            ) {
                 mainViewModel.onOpenKeyClicked()
             }
         }
@@ -115,6 +124,8 @@ fun App(mainViewModel: MainViewModel) {
 fun KeyWindow(
     initialState: Boolean,
     key: String,
+    srcBytes: String,
+    resBytes: String,
     onCloseRequest: () -> Unit
 ) {
     val modifier = Modifier.fillMaxSize()
@@ -123,7 +134,7 @@ fun KeyWindow(
         visible = initialState,
         onCloseRequest = onCloseRequest,
         state = WindowState(
-            size = DpSize(300.dp, 600.dp)
+            size = DpSize(600.dp, 600.dp)
         )
     ) {
         Scaffold(
@@ -140,27 +151,54 @@ fun KeyWindow(
                 }
             }
         ) {
-            LazyColumn (
-                modifier = modifier
-                    .scrollable(
-                        state = rememberScrollableState{ 0f },
-                        orientation = Orientation.Vertical
+            Row(modifier = Modifier.fillMaxSize()) {
+                Column(
+                    modifier = Modifier.weight(0.33f)
+                ) {
+                    LazyColView(srcBytes, "Source")
+                }
+                Column(
+                    modifier = Modifier.weight(0.33f)
+                ) {
+                    LazyColView(key, "Key")
+                }
+                Column(
+                    modifier = Modifier.weight(0.33f)
+                ) {
+                    LazyColView(resBytes, "Result")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LazyColView(data: String, name: String) {
+    LazyColumn (
+        modifier = Modifier
+            .scrollable(
+                state = rememberScrollableState{ 0f },
+                orientation = Orientation.Vertical
+            )
+            .border(width = 1.dp, color = Color.Black)
+            .padding(5.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        item {
+            Text(name)
+        }
+        for (i in 8..data.length step 8) {
+            item {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = i.toString() + " -> [" + data.subSequence(i - 8, i) + "]",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.fillMaxWidth()
                     )
-            ) {
-                for (i in key.indices step 8) {
-                    item {
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.Center
-                        ) {
-                            Text(
-                                text = i.toString() + " -> [" +key.subSequence(i, i + 8).toString() + "]",
-                                fontSize = 20.sp,
-                                fontWeight = FontWeight.Bold,
-                                modifier = Modifier.fillMaxWidth()
-                            )
-                        }
-                    }
                 }
             }
         }
@@ -179,4 +217,6 @@ fun main() = application {
         App(mainViewModel = mainViewModel)
     }
     //11111111111111111111111111111111111111
+    //00000000111111111111111111111111111111
+
 }
